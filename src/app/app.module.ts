44fea8466490d26c68ed6d93e0ea17ff4c1c630c
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-06-20 23:21:35 +0630

    sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { SignupComponentComponent } from './components/signup-component/signup-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponentComponent,
+    SignupComponentComponent
   ],
   imports: [
     BrowserModule,
